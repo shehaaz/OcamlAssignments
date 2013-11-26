b0fd84d5d798fa5a0152f05ba355fd8cb338a59f
ataias <ataiasreis@gmail.com>
2013-11-25 19:06:56 -0500

    Question 3 working for some cases, still not finished

--- hw5/typecheck.ml
+++ hw5/typecheck.ml
@@ -1,21 +1,26 @@
 open Type
 module M = Minml
 open Unify
-  
+
 type context = Ctx of (M.name * tp) list
 
 let empty = Ctx []
 
 exception Unimplemented
-
 exception NotFound
+
+(*gets the type of a certain name given a list *)
 let rec assoc x y = match y with
   | [] -> raise NotFound
-  | (y, r)::rest -> if x = y then r else assoc x rest
+  | (z, r)::rest -> if x = z then r else assoc x rest
 
+(*Looks in a context and gets the type of the name*)
 let rec lookup ctx x = let Ctx list = ctx in assoc x list
+
+(*Adds an element to a context*)
 let extend ctx (x, v) = let Ctx list = ctx in Ctx ((x,v)::list)
 
+(*Adds several elements to a context*)
 let rec extend_list ctx l = match l with
   | [] -> ctx
   | (x,y) :: pairs -> extend_list (extend ctx (x, y)) pairs
@@ -32,9 +37,41 @@ let primopType p = match p with
   | M.Times    -> Arrow(Product[Int; Int], Int)
   | M.Negate   -> Arrow(Int, Int)
 
-(* Question 3. *)
+and  primopOutput p = match p with
+  | M.Equals   -> Bool
+  | M.LessThan -> Bool
+  | M.Plus     -> Int
+  | M.Minus    -> Int
+  | M.Times    -> Int
+  | M.Negate   -> Int
 
+and primopInput p = match p with
+  | M.Equals   -> [Int; Int]
+  | M.LessThan -> [Int; Int]
+  | M.Plus     -> [Int; Int]
+  | M.Minus    -> [Int; Int]
+  | M.Times    -> [Int; Int]
+  | M.Negate   -> [Int]
+
+(* Question 3. *)
+let msg = "Expression does not typecheck"
+let get y = match y with |Some k -> k | None -> fail "No annotation";;
 (* infer : context -> M.exp -> tp  *)
-let rec infer ctx exp = raise Unimplemented
+
+let rec infer ctx exp = match exp with
+  | M.Var y -> lookup ctx y
+  | M.Int n -> Int
+  | M.Bool b -> Bool
+  | M.If(e, e1, e2) -> if (infer ctx e)=Bool 
+                     then (let (x,y) = (infer ctx e1, infer ctx e2) in 
+			   if x = y then x else fail msg)
+		     else fail msg
+  | M.Primop (po, args) -> if (primopInput po) = (List.map (fun s->infer ctx s) args) then primopOutput po else fail msg
+  | M.Tuple exps -> Product (List.map (fun x -> infer ctx x) exps)
+  | M.Fn (x, t, e) -> let tt = get t in infer (extend ctx (x,tt)) e
+  | M.Rec (x, t, e) -> raise Unimplemented
+  | M.Let (decs, e2) -> raise Unimplemented
+  | M.Apply (e1, e2) -> raise Unimplemented
+  | M.Anno (e, t) -> raise Unimplemented
 
 
--- hw5/typecheck.mli
+++ hw5/typecheck.mli
@@ -2,5 +2,10 @@ type context  (* Context of typing assumptions; example: x : int, p : bool *)
 val empty : context
   
 val infer : context -> Minml.exp -> Type.tp
-
+(**)
+val assoc : 'a -> ('a * 'b) list -> 'b
+val lookup : context -> Minml.name -> Type.tp
+val extend : context -> (Minml.name * Type.tp) -> context (*Extend a context, adds an element to it*)
+val primopType : Minml.primop -> Type.tp
+(**)
 exception TypeError of string
